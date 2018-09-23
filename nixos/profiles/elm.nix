62e7d8adcce11bee25c4898b1d007537e4658b5f
Marek Fajkus <marek.faj@gmail.com>
2018-09-23 12:46:48 +0200

    bring in nix files

---
+++ nixos/profiles/elm.nix
@@ -0,0 +1,14 @@
+# In ideal world every project would have it's own nix defintion
+# this is hard to enforce so let's provide sane globals
+# So work can be done
+
+{ config, pkgs, ... }:
+
+let verify-examples = pkgs.callPackage ../pkgs/elm-verify-examples.nix {};
+in {
+    environment.systemPackages = with pkgs.elmPackages; [
+        elm
+        elm-format
+        # verify-examples
+    ];
+}
