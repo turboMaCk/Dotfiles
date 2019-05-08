34c3c8d32c22a0708ef3d4da2ee3211e25aac77f
Marek Fajkus <marek.faj@gmail.com>
2019-05-08 09:19:47 +0200

    add timezone

--- nixos/machines/thinkpad.nix
+++ nixos/machines/thinkpad.nix
@@ -46,7 +46,7 @@
   #};
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Prague";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
