d40eec91e60c6922cf261eb69492fae90682279f
Marek Fajkus <marek.faj@gmail.com>
2019-12-09 00:46:23 +0100

    desktop enable bluettoth

--- nixos/machines/desktop.nix
+++ nixos/machines/desktop.nix
@@ -102,9 +102,15 @@
   hardware.pulseaudio = {
     # Sound config
     enable = true;
+    package = pkgs.pulseaudioFull;
+    extraModules = [ pkgs.pulseaudio-modules-bt ];
     support32Bit = true;
   };
 
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+  };
 
   # services.xserver.imwheel = {
   #   enable = true;
