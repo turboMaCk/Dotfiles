fe094af4d5032f60f24faf154d3d0d9d43f97c65
Marek Fajkus <marek.faj@gmail.com>
2019-12-11 14:46:12 +0100

    latex plugin additions

--- nixos/profiles/latex.nix
+++ nixos/profiles/latex.nix
@@ -4,7 +4,22 @@
   environment.systemPackages = with pkgs; [
     ghostscript
     (pkgs.texlive.combine {
-        inherit (texlive) scheme-basic blindtext geometry;
+      inherit (texlive)
+        scheme-basic
+        blindtext
+        geometry
+        microtype
+        graphics
+        wrapfig
+        enumitem
+        fancyhdr
+        amsmath
+        index
+        titlesec
+        titling
+        opensans
+        xcolor
+      ;
     })
   ];
 }
