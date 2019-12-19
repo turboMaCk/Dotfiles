60038b385fb34825aa9c2279746185b0b2db502a
Marek Fajkus <marek.faj@gmail.com>
2019-12-19 11:53:09 +0100

    Enable imwheel service

--- nixos/machines/desktop.nix
+++ nixos/machines/desktop.nix
@@ -112,19 +112,19 @@
     enable = true;
   };
 
-  # services.xserver.imwheel = {
-  #   enable = true;
-  #   rules = {
-  #     "chromium-browser" = ''
-  #       None,      Up,   Button4, 8
-  #       None,      Down, Button5, 8
-  #       Shift_L,   Up,   Shift_L|Button4, 4
-  #       Shift_L,   Down, Shift_L|Button5, 4
-  #       Control_L, Up,   Control_L|Button4
-  #       Control_L, Down, Control_L|Button5
-  #     '';
-  #   };
-  # };
+  services.xserver.imwheel = {
+    enable = true;
+    rules = {
+      "chrom*|slack|discord|evolution|Firefox" = ''
+        None,      Up,   Button4, 8
+        None,      Down, Button5, 8
+        Shift_L,   Up,   Shift_L|Button4, 4
+        Shift_L,   Down, Shift_L|Button5, 4
+        Control_L, Up,   Control_L|Button4
+        Control_L, Down, Control_L|Button5
+      '';
+    };
+  };
 
   # Set hosts
   # networking.hosts."128.199.58.247" = [ "planning-game.com" ];
