a27e85fde8bdcaa0bb3a99a2137b5d4b03a4dfe8
Marek Fajkus <marek.faj@gmail.com>
2020-01-04 23:23:33 +0100

    remove outdated stuff

--- nixos/machines/thinkpad.nix
+++ nixos/machines/thinkpad.nix
@@ -5,24 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = pkgs: {
-    haskellPackages = pkgs.haskellPackages.override {
-      overrides = hsSelf: hsSuper: {
-        snap-server = pkgs.haskell.lib.overrideCabal hsSuper.snap-server (drv: {
-          patches = [
-            (pkgs.fetchpatch {
-              url = https://github.com/snapframework/snap-server/pull/126/commits/4338fe15d68e11e3c7fd0f9862f818864adc1d45.patch;
-              sha256 = "1nlw9lckm3flzkmhkzwc7zxhdh9ns33w8p8ds8nf574nqr5cr8bv";
-            })
-            (pkgs.fetchpatch {
-              url = https://github.com/snapframework/snap-server/pull/126/commits/410de2df123b1d56b3093720e9c6a1ad79fe9de6.patch;
-              sha256 = "08psvw0xny64q4bw1nwg01pkzh01ak542lw6k1ps7cdcwaxk0n94";
-            })
-          ];
-        });
-      };
-    };
-  };
   imports =
     [ <nixos-hardware/lenovo/thinkpad/t480s>
       # overrides
@@ -32,7 +14,7 @@
       ../profiles/desktop.nix
       ../users/marek.nix
       ../profiles/virtualization.nix
-      ../profiles/elm.nix
+      # ../profiles/elm.nix
       ../profiles/nodejs.nix
       ../profiles/haskell.nix
       ../profiles/purescript.nix
