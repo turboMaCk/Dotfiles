e391bbb8e0e94c3a74546881c4fcd65ee4c192dc
Marek Fajkus <marek.faj@gmail.com>
2020-02-07 17:48:56 +0100

    add miso cachix public key

--- nixos/profiles/haskell.nix
+++ nixos/profiles/haskell.nix
@@ -36,6 +36,7 @@ in
     binaryCachePublicKeys = [
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
       "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
+      "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
     ];
     trustedUsers = [ "root" "marek" ];
   };
