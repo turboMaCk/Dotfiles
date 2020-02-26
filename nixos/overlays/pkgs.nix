10e3cf49835c761ab02023491d83115f36857a0f
Marek Fajkus <marek.faj@gmail.com>
2020-02-26 10:40:29 +0100

    add discord overlay

--- nixos/overlays/pkgs.nix
+++ nixos/overlays/pkgs.nix
@@ -1,4 +1,11 @@
 self: super:
 {
   zasm = super.callPackage ../pkgs/zasm.nix {};
+  discord = super.discord.overrideAttrs (old: rec {
+    version = "0.0.10";
+    src = super.fetchurl {
+      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "0kx92i8naqr3algmyy3wyzbh8146z7gigxwf1nbpg1gl16wlplaq";
+    };
+  });
 }
