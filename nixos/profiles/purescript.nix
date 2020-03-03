12fe4dd43f7884f532707efe33020216164c3674
Marek Fajkus <marek.faj@gmail.com>
2020-03-03 14:55:26 +0100

    update purescript

--- nixos/profiles/purescript.nix
+++ nixos/profiles/purescript.nix
@@ -3,20 +3,9 @@
 # So work can be done
 
 { pkgs, ... }:
-let
-  easyPS = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "easy-purescript-nix";
-    rev = "993f63359b64db080061b274e4688e3b80c4f68e";
-    sha256 = "18b7fmmxkg38y1av9kfgcv2rikdlji51ya5b9p7sy3aml2hprmi5";
-  });
-in {
-
-# inherit(easyPS.inputs);
-  environment.systemPackages = with easyPS; [
-    purs
-    psc-package-simple
-    purp
+{
+  environment.systemPackages = with pkgs; [
     spago
+    purescript
   ];
 }
