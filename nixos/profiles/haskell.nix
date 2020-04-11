d5f0416bc64773f991c0f9c6ac0595198358d55d
Marek Fajkus <marek.faj@gmail.com>
2020-04-11 17:12:21 +0200

    update prezto

--- nixos/profiles/haskell.nix
+++ nixos/profiles/haskell.nix
@@ -10,7 +10,7 @@ in
   environment.systemPackages = with pkgs.haskellPackages; [
     pkgs.cabal-install
     pkgs.cabal2nix
-    # hindent
+    hlint
     stylish-haskell
     hasktags
     ghcid
@@ -30,12 +30,12 @@ in
     binaryCaches = [
       "https://cache.nixos.org/"
       "https://nixcache.reflex-frp.org"
-      "https://all-hies.cachix.org"
+      # "https://all-hies.cachix.org"
       "https://miso-haskell.cachix.org"
     ];
     binaryCachePublicKeys = [
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-      "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
+      # "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
       "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
     ];
     trustedUsers = [ "root" "marek" ];
