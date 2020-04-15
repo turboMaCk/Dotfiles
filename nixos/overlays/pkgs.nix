8a3c3f3e65ed772e51dce47e57492c4d8fe0ab77
Marek Fajkus <marek.faj@gmail.com>
2020-04-15 10:19:42 +0200

    add create-elm-app overlay

--- nixos/overlays/pkgs.nix
+++ nixos/overlays/pkgs.nix
@@ -14,4 +14,14 @@ self: super:
   #   - https://github.com/input-output-hk/haskell.nix/issues/537#issuecomment-611322396
   #   - https://github.com/NixOS/nixpkgs/issues/67032#issuecomment-607732200
   liblapack = super.liblapack.override { shared = true; };
+
+  elmPackages = super.elmPackages // {
+    create-elm-app = super.elmPackages.create-elm-app.override (old: {
+      nativeBuildInputs = old.nativeBuildInputs or [] ++ [ self.makeWrapper ];
+      postInstall = old.postInstall + ''
+        ln -sf ${self.elmPackages.elm}/bin/elm $out/lib/node_modules/create-elm-app/node_modules/elm/bin
+        wrapProgram $out/bin/elm-app --suffix PATH : ${self.elmPackages.elm}/bin
+      '';
+    });
+  };
 }
