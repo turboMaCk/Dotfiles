f4f41845484b9f7e2b701309e90f3f8c7dfffa54
Marek Fajkus <marek.faj@gmail.com>
2020-05-17 17:06:51 +0200

    fix compatibility (opacity to float)

--- nixos/profiles/desktop.nix
+++ nixos/profiles/desktop.nix
@@ -87,12 +87,12 @@
     fade            = true;
     shadow          = true;
     fadeDelta       = 4;
-    shadowOpacity   = "0.5";
+    shadowOpacity   = 0.5;
 
     # opacity
-    activeOpacity   = "1.0";
-    inactiveOpacity = "1.0";
-    menuOpacity     = "1.0";
+    activeOpacity   = 1.0;
+    inactiveOpacity = 1.0;
+    menuOpacity     = 1.0;
 
     # OpenGL
     backend         = "glx";
