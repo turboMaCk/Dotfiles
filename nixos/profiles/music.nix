d497b8e7ecf4df851793a9ef8bac73882b6b2435
Marek Fajkus <marek.faj@gmail.com>
2021-03-10 01:52:54 +0100

    make jack optional audio server

--- nixos/profiles/music.nix
+++ nixos/profiles/music.nix
@@ -3,6 +3,9 @@
   environment.systemPackages = with pkgs; [
     renoise
     qjackctl
+    jack2
   ];
+  # I want to use pulse audio as a primary
+  # sounds server. jack is on demand
   # services.jack.jackd.enable = true;
 }
