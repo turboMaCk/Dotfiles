cc6162023b54be16edc328584ea246dd6727452e
Marek Fajkus <marek.faj@gmail.com>
2022-08-27 15:50:16 +0200

    thinkpad updates

--- nixos/machines/thinkpad.nix
+++ nixos/machines/thinkpad.nix
@@ -18,12 +18,12 @@
       ../profiles/haskell.nix
       ../profiles/purescript.nix
       ../profiles/rust.nix
-      ../profiles/ocaml.nix
+      # ../profiles/ocaml.nix
       ../profiles/printing.nix
       ../profiles/holmusk.nix
       ../profiles/vpn.nix
       ../profiles/samba.nix
-      ../profiles/music.nix
+      # ../profiles/music.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
