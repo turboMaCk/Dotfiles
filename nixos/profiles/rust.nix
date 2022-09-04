3e00a6c05a902e9c12e2d39bd59e8de051d3da68
Marek Fajkus <marek.faj@gmail.com>
2022-09-04 12:13:41 +0200

    add rustc and ditch rustup from global env

--- nixos/profiles/rust.nix
+++ nixos/profiles/rust.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
 
 { environment.systemPackages = with pkgs; [
+    rustc
     cargo
     cargo-generate
     cargo-web
-    rustup
+    # rustup # pulls incompatible binaries 2022-09-04
 
     # tools
     rust-analyzer
