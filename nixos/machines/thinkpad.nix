f5e94c5d5075f5e8d0431f519241f0f7b3db56e5
Marek Fajkus <marek.faj@gmail.com>
2024-08-18 12:41:15 +0200

    remove opentofu from global deps

--- nixos/machines/thinkpad.nix
+++ nixos/machines/thinkpad.nix
@@ -70,7 +70,7 @@
   ];
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+ networking.firewall.allowedTCPPorts = [ 8000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
--- nixos/profiles/holmusk.nix
+++ nixos/profiles/holmusk.nix
@@ -44,11 +44,6 @@ in {
     # pkgs.yubico-pam
     # pkgs.yubico-piv-tool
 
-    # deployment
-    # using manually downloaded binary to get the right version
-    # pkgs.terraform
-    pkgs.packer
-    pkgs.opentofu
     pkgs.ssm-session-manager-plugin
   ];
 }
